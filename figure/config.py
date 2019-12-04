9f896e9ad7d93357f6773b3a2f113b2997f690e5
Daniele Ongari <daniele.ongari@gmail.com>
2019-12-04 20:34:06 +0100

    figure working with 66k cofs

---
+++ figure/config.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+import collections
+import yaml
+from os.path import join, dirname
+
+static_dir = join(dirname(__file__), "static")
+
+with open(join(static_dir, "columns.yml"), 'r') as f:
+    quantity_list = yaml.load(f)
+
+for item in quantity_list:
+    if 'scale' not in item.keys():
+        item['scale'] = 'linear'
+
+
+quantities = collections.OrderedDict([(q['column'], q) for q in quantity_list])
+
+plot_quantities = [
+    q for q in quantities.keys() if quantities[q]['type'] == 'float'
+]
+
+with open(join(static_dir, "filters.yml"), 'r') as f:
+    filter_list = yaml.load(f)
+
+with open(join(static_dir, "presets.yml"), 'r') as f:
+    presets = yaml.load(f)
+
+for k in presets.keys():
+    if 'clr' not in list(presets[k].keys()):
+        presets[k]['clr'] = presets['default']['clr']
+
+max_points = 70000
---
+++ figure/query.py
@@ -0,0 +1,48 @@
+"""Querying the DB
+"""
+from __future__ import absolute_import
+from bokeh.models.widgets import RangeSlider, CheckboxButtonGroup
+# pylint: disable=too-many-locals
+data_empty = dict(x=[0], y=[0], uuid=['1234'], color=[0], name=['no data'])
+
+get_tag = {
+    'PE': 'pe_out',
+    'WCg': 'pe_out',
+    'WCv': 'pe_out',
+    'Pur': 'pe_out',
+    'Density': 'ht_geom_out',
+    'Channels.Largest_included_spheres.0': 'ht_geom_out',
+    'Channels.Largest_free_spheres.0': 'ht_geom_out',
+    'AV_Volume_fraction': 'ht_geom_out',
+    'henry_coefficient_average_ht': 'ht_kh_out2',
+    'henry_coefficient_average_co2': 'isot_co2_out',
+    'henry_coefficient_average_n2': 'isot_n2_out',
+    'POAV_Volume_fraction': 'isot_co2_out',
+    'POAV_cm^3/g': 'isot_co2_out',
+}
+
+def get_data_aiida(inp_list):
+    """Query the AiiDA database
+
+    TODO: this section needs to be fixed if more version are present for the same COF!
+    TODO: add version search!
+    """
+    from aiida.orm.querybuilder import QueryBuilder
+    from aiida.orm import Node, Dict, Group, CifData
+
+    filters = {}
+
+    qb = QueryBuilder()
+    qb.append(Group, filters={ 'label': {'like': 'group_%'} }, tag='group')
+    qb.append(CifData, with_group='group', filters={'extras.group_tag': 'orig_cif'},
+        project=['label'])
+
+    for inp in inp_list:
+        if 'henry_coefficient_average' in inp:
+            proj = 'henry_coefficient_average' # take out _co2, _n2, _ht
+        else:
+            proj = inp
+        qb.append(Dict, with_group='group', filters={'extras.group_tag': get_tag[inp]},
+            project=['attributes.{}'.format(proj)])
+
+    return qb.all()
